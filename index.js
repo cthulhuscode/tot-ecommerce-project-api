9c9d543d2416026b4503f65b22ee175dd02011d8
Enrique Ruvalcaba <jordison352@gmail.com>
2023-04-08 12:20:08 -0600

    trying to fix deployment

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-import express, { json, urlencoded } from "express";
 import dotenv from "dotenv";
+dotenv.config();
+import express, { json, urlencoded } from "express";
 import cors from "cors";
 import storeRouter from "./routers/store.js";
 import authRouter from "./routers/auth.js";
-
-// if (process.env.NODE_ENV !== "production") dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
